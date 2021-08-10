b8ac009344d3eb9d4e0c6ebdea75f57ff55f898d
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-08-10 18:15:26 -0500

    Create components, service & models

---
+++ src/app/models/TarjetaCredito.ts
@@ -0,0 +1,20 @@
+import { threadId } from "worker_threads";
+
+export class TarjetaCredito{
+    id?: string;
+    titular: string;
+    numeroTarjeta: string;
+    fechaExpiracion: string;
+    cvv: number;
+    fechaCreacion: Date;
+    fechaActualizacion: Date;
+    
+    constructor(titular: string, numeroTarjeta: string, fechaExpiracion: string, cvv: number){
+        this.titular = titular;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaExpiracion = fechaExpiracion;
+        this.cvv = cvv;
+        this.fechaCreacion = new Date();
+        this.fechaActualizacion = new Date();
+    }
+}
